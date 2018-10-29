41477bc5057a233a596d791c6e49688cf05c19bf
carissa morrow <carissamorrow@hotmail.com>
2018-10-29 15:17:48 -0600

    lenses

---
+++ main.js
@@ -0,0 +1,19 @@
+let glasses = {
+  lenses: "two",
+  shape: "round",
+  size: "small"
+}
+let frame = {
+  color: "tan",
+  weight: "light",
+  texture: "smooth"
+}
+let lenses = {
+  clarity: "clear",
+  tint: "none"
+}
+let glasses = {
+  frame,
+  lenses
+}
+console.log(glasses.frame)
